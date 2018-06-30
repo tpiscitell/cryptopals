2c9e0fc9e0c0e556b2087a97575503ba693cd705
Tom Piscitell <tom.piscitell@gmail.com>
2018-06-30 19:23:53 -0400

    challenge three

--- main.go
+++ main.go
@@ -1,5 +1,67 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
 func main() {
-	println("These are the CryptoPal Challenges")
+	app := cli.NewApp()
+
+	app.Name = "cryptopals"
+	app.Usage = "The CryptoPals Challenges"
+	app.Commands = []cli.Command{
+		{
+			Name:  "one",
+			Usage: "Hex to Base64",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "input",
+					Value: "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d",
+				},
+			},
+			Action: func(c *cli.Context) error {
+				out := ChallengeOne(c.String("input"))
+				println(out)
+				return nil
+			},
+		},
+		{
+			Name:  "two",
+			Usage: "Fixed length XOR",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "input",
+					Value: "1c0111001f010100061a024b53535009181c",
+				},
+				cli.StringFlag{
+					Name:  "key",
+					Value: "686974207468652062756c6c277320657965",
+				},
+			},
+			Action: func(c *cli.Context) error {
+				println(ChallengeTwo(c.String("input"), c.String("key")))
+				return nil
+			},
+		},
+		{
+			Name:  "three",
+			Usage: "Hex to Base64",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "input",
+					Value: "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736",
+				},
+			},
+			Action: func(c *cli.Context) error {
+				out := ChallengeThree(c.String("input"))
+				fmt.Println(out)
+				return nil
+			},
+		},
+	}
+
+	app.Run(os.Args)
 }
--- setone.go
+++ setone.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/base64"
 	"encoding/hex"
+	"errors"
 )
 
 func ChallengeOne(in string) string {
@@ -37,4 +38,86 @@ func ChallengeTwo(in, key string) string {
 	}
 
 	return hex.EncodeToString(outBytes)
+}
+
+// Challenge Three
+
+func singleCharXOR(in []byte, key byte) ([]byte, error) {
+	n := len(in)
+	out := make([]byte, n)
+
+	if n != len(out) {
+		return nil, errors.New("out and in must be the same length")
+	}
+
+	for i := 0; i < n; i++ {
+		out[i] = in[i] ^ key
+	}
+
+	return out, nil
+}
+
+func scoreString(in []byte) float64 {
+	expectedFreqs := []float64{
+		0.0651738, 0.0124248, 0.0217339, 0.0349835, //'A', 'B', 'C', 'D',...
+		0.1041442, 0.0197881, 0.0158610, 0.0492888,
+		0.0558094, 0.0009033, 0.0050529, 0.0331490,
+		0.0202124, 0.0564513, 0.0596302, 0.0137645,
+		0.0008606, 0.0497563, 0.0515760, 0.0729357,
+		0.0225134, 0.0082903, 0.0171272, 0.0013692,
+		0.0145984, 0.0007836, 0.1918182, //'Y', 'Z', ' '
+	}
+
+	charCounts := make([]int, 27)
+	charFreqs := make([]float64, 27)
+	totalCount := 0
+
+	for _, c := range in {
+		//to lower all the ascii capital letters
+		if 'A' <= c && c <= 'Z' {
+			c -= 32
+		}
+
+		if 'a' <= c && c <= 'z' {
+			charCounts[int(c)-'a']++
+			totalCount++
+		}
+
+		if c == ' ' {
+			charCounts[26]++
+			totalCount++
+		}
+	}
+
+	if totalCount == 0 {
+		return 0
+	}
+
+	var chi2 float64
+	for i := 0; i < 27; i++ {
+		charFreqs[i] = float64(charCounts[i]) / float64(totalCount)
+		chi2 += (charFreqs[i] - expectedFreqs[i]) * (charFreqs[i] - expectedFreqs[i]) / expectedFreqs[i]
+	}
+
+	return chi2
+}
+
+func ChallengeThree(in string) string {
+	inBytes, _ := hex.DecodeString(in)
+
+	minScore := 100000.00
+	var answer string
+
+	for i := 0; i < 256; i++ {
+		k := byte(i)
+		outBytes, _ := singleCharXOR(inBytes, k)
+		score := scoreString(outBytes)
+
+		if score > 1 && score < minScore {
+			minScore = score
+			answer = string(outBytes)
+		}
+	}
+
+	return answer
 }
